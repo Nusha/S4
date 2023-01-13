adf70ca6a7101b411bf235a26a5fa102f2dd6d88
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 18:47:13 +0300

    TeacherController added

--- controller/TeacherController.java
+++ controller/TeacherController.java
@@ -1,4 +1,27 @@
 package Sem_02.controller;
 
-public class TeacherController {
+import Sem_02.data.Teacher;
+
+public class TeacherController implements UserController<Teacher, Integer> {
+
+    public final TeacherController teacherController;
+
+    public TeacherController(TeacherController teacherController) {
+        this.teacherController = teacherController;
+    }
+
+    @Override
+    public Teacher save(Teacher entry) {
+        return teacherController.save(entry);
+    }
+
+    @Override
+    public Teacher findById(Integer id) {
+        return teacherController.findById(id);
+    }
+
+    @Override
+    public Teacher findByName(String name) {
+        return teacherController.findByName(name);
+    }
 }
