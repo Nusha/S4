5cd4773c900cdd908f32b2a11c47040080e0df2a
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 18:41:25 +0300

    Controller interfaces added.

--- controller/Controller.java
+++ controller/Controller.java
@@ -3,5 +3,5 @@ package Sem_02.controller;
 public interface Controller <E, I>{
     E save(E entry);
     E findById(I id);
-    E findByFio(E fio);
+
 }
---
+++ controller/UserController.java
@@ -0,0 +1,7 @@
+package Sem_02.controller;
+
+import Sem_02.data.User;
+
+public interface UserController <E extends User, I> extends Controller<E, I>{
+    E findByName(String name);
+}
