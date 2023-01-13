065d22ef191072bc35a4e61367afae72de49ccd7
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 17:52:03 +0300

    TeacherRepository class added.

---
+++ repository/TeacherRepository.java
@@ -0,0 +1,20 @@
+package Sem_02.repository;
+
+import Sem_02.data.Teacher;
+
+public class TeacherRepository implements UserRepository<Teacher,Integer> {
+    @Override
+    public Teacher save(Teacher entry) {
+        return null;
+    }
+
+    @Override
+    public Teacher findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Teacher findByFio(String fio) {
+        return null;
+    }
+}
