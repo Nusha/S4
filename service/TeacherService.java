5a118368f4bdb1740c6a387295ef1dc69bdb573a
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 18:06:14 +0300

    TeacherService methods added.

--- service/TeacherService.java
+++ service/TeacherService.java
@@ -1,4 +1,24 @@
 package Sem_02.service;
 
+import Sem_02.data.Teacher;
+import Sem_02.repository.TeacherRepository;
+
 public class TeacherService {
+    private final TeacherRepository teacherRepository;
+
+    public TeacherService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
+    public Teacher saveTeacher(Teacher name) {
+        return teacherRepository.save(name);
+    }
+
+    public Teacher findTeacherByID(int id) {
+        return teacherRepository.findById(id);
+    }
+
+    public Teacher findTeacherByFio(String fio) {
+        return teacherRepository.findByFio(fio);
+    }
 }
