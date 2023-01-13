65ae146b478c87fa358467a49fb1424d0522bb6e
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 17:56:51 +0300

    Unnecessary comment deleted

--- service/StudentGroupService.java
+++ service/StudentGroupService.java
@@ -4,10 +4,6 @@ import Sem_02.data.Student;
 import Sem_02.data.StudentGroup;
 
 public interface StudentGroupService {
-    /** Работа над ошибками 2 урок.
-     * Создать интерфейс с абстрактным методом StudentGroup createGroup(int groupNumber).
-     * Создал абстрактный метод StudentGroup createGroup(int groupNumber).
-     */
     StudentGroup createGroup(int groupNumber);
 
     StudentGroup getGroup();
