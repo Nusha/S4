513d3fe348fe4014cb68c32dfb1b300adb087a69
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 19:32:03 +0300

    Groupstream functions added

--- service/GroupStreamService.java
+++ service/GroupStreamService.java
@@ -2,7 +2,12 @@ package Sem_02.service;
 
 import java.util.List;
 
-public interface GroupStreamService {
+public interface GroupStreamService<E, I> {
 
     void sortStream(List groupStream);
+
+
+    E saveStream(E entity);
+
+    E findStreamByID(I entity);
 }
--- service/GroupStreamServiceImpl.java
+++ service/GroupStreamServiceImpl.java
@@ -1,15 +1,37 @@
 package Sem_02.service;
 
+import Sem_02.data.GroupStream;
 import Sem_02.data.comparator.GroupStreamComparator;
+import Sem_02.repository.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
-public class GroupStreamServiceImpl implements GroupStreamService{
+public class GroupStreamServiceImpl implements GroupStreamService<GroupStream, Integer> {
+    private final Repository<GroupStream, Integer> gsRepository;
+
+    public GroupStreamServiceImpl(Repository<GroupStream, Integer> gsRepository) {
+        this.gsRepository = gsRepository;
+    }
 
     @Override
     public void sortStream(List groupStream) {
         Collections.sort(groupStream, new GroupStreamComparator());
 
     }
+
+    @Override
+    public GroupStream saveStream(GroupStream entity) {
+        return gsRepository.save(entity);
+    }
+
+    @Override
+    public GroupStream findStreamByID(Integer id) {
+        return gsRepository.findById(id);
+    }
+
+
 }
+
+
+
