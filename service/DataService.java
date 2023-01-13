f273959210b14ad47f5b9b05410830c60d34ed49
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 17:57:39 +0300

    Generic, extending User added to interface.

--- service/DataService.java
+++ service/DataService.java
@@ -3,7 +3,7 @@ package Sem_02.service;
 import Sem_02.data.Student;
 import Sem_02.data.User;
 
-public interface DataService {
+public interface DataService<E extends User> {
     void create(User user);
     User read(User user); // Удалил дефолтные методы. Добавил абстрактные.
 
